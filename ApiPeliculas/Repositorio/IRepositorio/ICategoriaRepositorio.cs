48e986c1933196f6186b245f5f54a527c7f9a0f1
Eisner Acevedo <Eisner.Acevedo@forsyteit.com>
2023-08-24 21:11:33 -0600

    Implementacion de la Interfaz de ICategoriaReporitorio

---
+++ ApiPeliculas/Repositorio/IRepositorio/ICategoriaRepositorio.cs
@@ -0,0 +1,17 @@
+﻿using ApiPeliculas.Modelos;
+
+namespace ApiPeliculas.Repositorio.IRepositorio
+{
+    public interface ICategoriaRepositorio
+    {
+        ICollection<Categoria> GetCategorias();
+        Categoria GetCategoria(int CategoriaId);
+        bool ExisteCategoria(string Nombre);
+        bool ExisteCategoria(int Id);
+        bool CrearCategoria(Categoria _Categoria);
+        bool ActualizarCategoria(Categoria _Categoria);
+        bool BorrarCategoria(Categoria _Categoria);
+        bool Guardar();
+
+    }
+}
