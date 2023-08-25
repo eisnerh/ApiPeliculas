6cffb0c2f2b2486161cda729003b9b69ae81f741
Eisner Acevedo <Eisner.Acevedo@forsyteit.com>
2023-08-24 20:14:03 -0600

    Crear Modelo Categoria

---
+++ ApiPeliculas/Modelos/Categoria.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiPeliculas.Modelos
+{
+    public class Categoria
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
