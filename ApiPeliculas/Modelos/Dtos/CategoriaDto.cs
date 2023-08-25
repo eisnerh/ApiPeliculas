02410c0f554eb18943569d04f6760115a57fa7a2
Eisner Acevedo <Eisner.Acevedo@forsyteit.com>
2023-08-24 21:34:21 -0600

    Implementar Repositorio CategoriaRepositorio

---
+++ ApiPeliculas/Modelos/Dtos/CategoriaDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiPeliculas.Modelos.Dtos
+{
+    public class CategoriaDto 
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [MaxLength(50, ErrorMessage = "El nombre no puede tener mas de 50 caracteres")]
+        public required string Nombre { get; set; }
+        public DateTime FechaCreacion { get; set;}
+    }
+}
---
+++ ApiPeliculas/Repositorio/CategoriaRepositorio.cs
@@ -0,0 +1,63 @@
+﻿using ApiPeliculas.Data;
+using ApiPeliculas.Modelos;
+using ApiPeliculas.Repositorio.IRepositorio;
+
+namespace ApiPeliculas.Repositorio
+{
+    public class CategoriaRepositorio : ICategoriaRepositorio
+    {
+        private readonly ApplicationDbContext _Bd;
+        public CategoriaRepositorio(ApplicationDbContext Bd)
+        {
+            _Bd = Bd;
+        }
+
+        public bool ActualizarCategoria(Categoria _Categoria)
+        {
+            _Categoria.FechaCreacion = DateTime.Now;
+            _Bd.Categoria.Update(_Categoria);
+            return Guardar();
+        }
+
+        public bool BorrarCategoria(Categoria _Categoria)
+        {
+            _Bd.Categoria.Remove(_Categoria);
+            return Guardar();
+        }
+
+        public bool CrearCategoria(Categoria _Categoria)
+        {
+            _Categoria.FechaCreacion = DateTime.Now;
+            _Bd.Categoria.Add(_Categoria);
+            return Guardar();
+        }
+
+        public bool ExisteCategoria(string Nombre)
+        {
+            bool Valor = _Bd.Categoria.Any(c => c.Nombre.ToLower().Trim() == Nombre.ToLower().Trim());
+            return Valor;
+        }
+
+        public bool ExisteCategoria(int Id)
+        {
+            return _Bd.Categoria.Any(c => c.Id == Id);
+
+        }
+
+        public Categoria GetCategoria(int CategoriaId)
+        {
+            return _Bd.Categoria.FirstOrDefault(c => c.Id == CategoriaId);
+
+        }
+
+        public ICollection<Categoria> GetCategorias()
+        {
+            return _Bd.Categoria.OrderBy(c => c.Nombre).ToList();
+        }
+
+        public bool Guardar()
+        {
+            return _Bd.SaveChanges() >= 0;
+        }
+    }
+}
